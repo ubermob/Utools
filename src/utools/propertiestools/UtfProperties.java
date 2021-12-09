58e9fca3c7fe193121259b9f8661f080dbe49cd5
uber <savg@mail.ru>
2021-12-09 23:24:23 +0300

    Ver. 1.4. UtfProperties finally changed.

--- src/utools/propertiestools/UtfProperties.java
+++ src/utools/propertiestools/UtfProperties.java
@@ -1,6 +1,9 @@
 package utools.propertiestools;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
@@ -8,21 +11,35 @@ import java.util.Properties;
  * Short call UTF-8 encoded {@link Properties}.
  *
  * @author Andrey Korneychuk on 24-Sep-21
- * @version 2.0
+ * @version 2.1
  */
 public class UtfProperties {
 
 	/**
-	 * Short call UTF-8 encoded {@link Properties}.
+	 * Short call UTF-8 encoded {@link Properties} from external environment.
 	 *
-	 * @param path to properties file
+	 * @param path to external properties file
 	 * @return ready for work properties
 	 * @throws IOException
 	 */
-	public static Properties asd(String path) throws IOException {
+	public static Properties getExternalUtfProperties(String path) throws IOException {
 		Properties properties = new Properties();
-		Reader inputStreamReader = new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8);
-		properties.load(inputStreamReader);
+		Reader reader = new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8);
+		properties.load(reader);
+		return properties;
+	}
+
+	/**
+	 * Short call UTF-8 encoded {@link Properties} from internal environment.
+	 *
+	 * @param path to internal properties file
+	 * @return ready for work properties
+	 * @throws IOException
+	 */
+	public static Properties getInternalUtfProperties(String path) throws IOException {
+		Properties properties = new Properties();
+		Reader reader = new InputStreamReader(UtfProperties.class.getResourceAsStream(path), StandardCharsets.UTF_8);
+		properties.load(reader);
 		return properties;
 	}
 }
